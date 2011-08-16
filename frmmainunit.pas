d4ed80e917553e56269b9329222b5eb3c7e3aef7
Gustavo Carreno <guscarreno@gmail.com>
2011-08-16 19:08:51 +0100

    Initial GUI layout sketched.

--- frmmainunit.pas
+++ frmmainunit.pas
@@ -1,14 +1,41 @@
-unit frmMainUnit; 
+unit frmMainUnit;
 
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  Menus, ActnList, StdActns, ExtCtrls, PairSplitter, StdCtrls;
 
 type
-  TForm1 = class(TForm)
+
+  { TfrmMainForm }
+
+  TfrmMainForm = class(TForm)
+    actFileNew: TAction;
+    actFileOpen: TAction;
+    actFileSave: TAction;
+    alApplication: TActionList;
+    actFileExit: TFileExit;
+    GroupBox1: TGroupBox;
+    mmApplication: TMainMenu;
+    mnuFileExit: TMenuItem;
+    mnuFileSep1: TMenuItem;
+    mnuFileSep2: TMenuItem;
+    mnuFileSave: TMenuItem;
+    mnuFileOpen: TMenuItem;
+    mnuFile: TMenuItem;
+    mnuFileNew: TMenuItem;
+    PaintBox1: TPaintBox;
+    pcSequences: TPageControl;
+    psApplication: TPairSplitter;
+    psSideLeft: TPairSplitterSide;
+    psSideRight: TPairSplitterSide;
+    sbApplication: TStatusBar;
+    Splitter1: TSplitter;
+    tsOption: TTabSheet;
+    tsSequences: TTabSheet;
   private
     { private declarations }
   public
@@ -16,7 +43,7 @@ type
   end; 
 
 var
-  Form1: TForm1; 
+  frmMainForm: TfrmMainForm;
 
 implementation
 
